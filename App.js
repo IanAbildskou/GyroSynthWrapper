4a941b7660cd254b3046ffe5383f861c6976c1f3
hcarreras <haricarreras@gmail.com>
2020-01-18 20:37:04 +0100

    wip

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { DeviceMotion } from 'expo-sensors';
+
+onMotionDetected = (webview, data) => {
+  if(webview){
+    webview.postMessage(JSON.stringify(data));
+  }
+}
 
 export default function App() {
-  return <WebView source={{ uri: 'https://gyrosynth.ianabildskou.now.sh/' }} />;
+  let webview = null
+
+  DeviceMotion.isAvailableAsync().then((isAvailable) =>{
+    if(!isAvailable) console.error("Device motion not available")
+  })
+
+  DeviceMotion.addListener((data) => onMotionDetected(webview, data))
+
+  return <WebView
+    source={{ uri: 'https://a13736e1.ngrok.io' }}
+    ref={( view ) => webview = view}
+  />
 }
 
 const styles = StyleSheet.create({
